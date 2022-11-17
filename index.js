4eb85fa8bf1279cbc68db79dfba643500bb75854
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-17 16:00:42 -0600

    connected app to express_started to make connection to database employee

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'Batman-robin#136!',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employer_db database.`)
+  );
+
+  db.query('', function (err, results) {
+    console.log(results);
+  })
+
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
