17b3c43ec029974a82eb3e83aa3c91efbe324c4e
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-18 17:26:32 -0600

    seeds SQL data completed

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,4 +10,20 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('')
+('Lead engineer', 120000, 1),
+('Human Resources Representative', 100000, 2),
+('Accountant', 75000, 3),
+('Lawyer', 180000, 4),
+('Salesperson', 75000, 5),
+
+
+INSERT INTON employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Alfred', 'Pennyworth', 1, NULL),
+('Bat', 'Man', 2, 1),
+('Barbra', 'Gordon', 3, 1),
+('Zelda', 'Hyrule', 4, NULL),
+('Link', 'NA', 5, 4),
+('Anakin', 'Skywalker', 1, NULL),
+('Ahsoka', 'Tano', 2, 1)
+
