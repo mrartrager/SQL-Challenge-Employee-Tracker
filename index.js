dcb679897bf0fb86a9f46c588d961f3e827f3916
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-18 17:03:32 -0600

    table format created in schema

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-
+const dotenv = require('dotenv');
 
 const PORT = process.env.PORT || 5001;
 const app = express();
@@ -8,20 +8,29 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+dotenv.config();
 
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: 'Batman-robin#136!',
-      database: 'employee_db'
+      host: 'localhost', 
+      user: 'root', // process.DB.USER
+      password: 'Batman-robin#136!', // process.DB_PASSWORD
+      database: 'employee_db' // process.DB_NAME
     },
     console.log(`Connected to the employee_db database.`)
   );
 
-  db.query('', function (err, results) {
-    console.log(results);
-  })
+//   db.query('', function (err, results) {
+//     console.log(results);
+//   })
+
+const init = () => {
+    inquirer.prompt({
+        type: 'input',
+        name: 'Department_id',
+        message: 'Which '
+    })
+}
 
 
   app.listen(PORT, () => {
