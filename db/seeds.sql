7810df752b99a6dc2d9337f03bbe2ed0c38965da
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-11-18 17:08:32 -0600

    started seedsSQL file

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE employee_db
+
+INSERT INTO department (name)
+VALUES 
+('Engineering'),
+('Human Resources'),
+('Finance'),
+('Legal'),
+('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('')
